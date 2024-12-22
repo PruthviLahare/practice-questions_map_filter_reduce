71e4410f66497d37a02b8f056d679a9c3e25a93a
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2024-12-22 21:01:30 +0530

    filterLongWords added

--- filter_chunks.js
+++ filter_chunks.js
@@ -1,3 +1,20 @@
+// words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
+
+const lengthGreaterThan5 = function (word) {
+  return word.length > 5;
+};
+
+const filterLongWords = function (words) {
+  return words.filter(lengthGreaterThan5);
+};
+
+const words = ["apple", "banana", "kiwi", "grape"];
+console.log(filterLongWords(words));
+
+
+console.log = function () { };
+// -----------------------------------------------------------------------------
+
 // even numbers [1, 2, 3, 4, 5] => [2, 4]
 
 const isEven = function (num) {
